4169c2bef38d2804eaf6e6d3ee6afd9474781dfd
kker5 <QpwkZ4i=MDRCwa4vvJH/eW>
2021-09-09 01:01:22 +0900

    Modify Application 이름 수정

--- src/main/java/me/benny/fcp/PointManagementApplication.java
+++ src/main/java/me/benny/fcp/PointManagementApplication.java
@@ -5,15 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 포인트 만료 시스템
+ * 포인트 관리 시스템
  * ex)
  * java -jar batch.jar --job.name=expirePointJob today=2021-01-01
  */
 @SpringBootApplication
 @Slf4j
-public class ExpirePointApplication {
+public class PointManagementApplication {
     public static void main(String[] args) {
         log.info("application arguments : " + String.join(",", args));
-        SpringApplication.run(ExpirePointApplication.class, args);
+        SpringApplication.run(PointManagementApplication.class, args);
     }
 }
