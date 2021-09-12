dc2907928a52ecf1ab00c8b7d9a020b8e0e7e1c4
kker5 <QpwkZ4i=MDRCwa4vvJH/eW>
2021-09-12 14:43:12 +0900

    Modify 테스트 설정을 application.yml로 이동

--- src/test/java/me/benny/fcp/job/expire/ExpirePointJobConfigurationTest.java
+++ src/test/java/me/benny/fcp/job/expire/ExpirePointJobConfigurationTest.java
@@ -26,31 +26,48 @@ class ExpirePointJobConfigurationTest extends BatchTestSupport {
 
     @Test
     void expirePointJob() throws Exception {
+        // given
         LocalDate earnDate = LocalDate.of(2021, 1, 1);
         LocalDate expireDate = LocalDate.of(2021, 1, 3);
-        LocalDate tomorrow = LocalDate.of(2021, 1, 4);
         PointWallet pointWallet = pointWalletRepository.save(
                 new PointWallet("user123", BigInteger.valueOf(6000))
         );
         pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, expireDate));
         pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, expireDate));
         pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, expireDate));
-        pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, expireDate, true, false));
-        pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, expireDate, false, true));
-        pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, tomorrow));
-        pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, tomorrow));
-        pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, tomorrow));
+        // when
         JobParameters jobParameters = new JobParametersBuilder()
                 .addString("today", "2021-01-04")
                 .toJobParameters();
         JobExecution execution = launchJob(expirePointJob, jobParameters);
+        // then
         List<Point> points = pointRepository.findAll();
         then(execution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED);
-        then(points.stream().filter(Point::isUsed)).hasSize(1);
-        then(points.stream().filter(Point::isExpired)).hasSize(4);
+        then(points.stream().filter(Point::isExpired)).hasSize(3);
         PointWallet changedPointWallet = pointWalletRepository.findById(pointWallet.getId()).orElseGet(null);
         then(changedPointWallet).isNotNull();
         then(changedPointWallet.getAmount()).isEqualByComparingTo(BigInteger.valueOf(3000));
+    }
+
+    @Test
+    void expirePointJob_이미사용한포인트() throws Exception {
+        // given
+        LocalDate earnDate = LocalDate.of(2021, 1, 1);
+        LocalDate expireDate = LocalDate.of(2021, 1, 3);
+        PointWallet pointWallet = pointWalletRepository.save(
+                new PointWallet("user123", BigInteger.valueOf(6000))
+        );
+        pointRepository.save(new Point(pointWallet, BigInteger.valueOf(1000), earnDate, expireDate, true, false));
+        // when
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("today", "2021-01-05")
+                .toJobParameters();
+        JobExecution execution = launchJob(expirePointJob, jobParameters);
+        // then
+        List<Point> points = pointRepository.findAll();
+        then(execution.getExitStatus()).isEqualTo(ExitStatus.COMPLETED);
+        then(points.stream().filter(Point::isUsed)).hasSize(1);
+        then(points.stream().filter(Point::isUsed).findFirst().get().isExpired()).isFalse();
     }
 
     @Test
