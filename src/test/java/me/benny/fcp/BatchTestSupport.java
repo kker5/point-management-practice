d4f228be47dc9a8c62ca276e49804b24a55a7cef
kker5 <QpwkZ4i=MDRCwa4vvJH/eW>
2021-09-12 12:06:05 +0900

    Modify job.enabled 설정을 부트 테스트 프로퍼티로 이동

--- src/test/java/me/benny/fcp/BatchTestSupport.java
+++ src/test/java/me/benny/fcp/BatchTestSupport.java
@@ -16,7 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = "spring.batch.job.enabled=false"
+)
 @ActiveProfiles("test")
 public abstract class BatchTestSupport {
 
